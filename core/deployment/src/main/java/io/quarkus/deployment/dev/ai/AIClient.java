c44acdc7ccf22a3f29c82b6fa41f6327d0d38aad
Phillip Kruger <phillip.kruger@gmail.com>
2025-02-25 11:11:18 +1100

    Rename to CRUD
    Signed-off-by: Phillip Kruger <phillip.kruger@gmail.com>

--- core/deployment/src/main/java/io/quarkus/deployment/dev/ai/AIClient.java
+++ core/deployment/src/main/java/io/quarkus/deployment/dev/ai/AIClient.java
@@ -7,33 +7,158 @@ import java.util.concurrent.CompletableFuture;
 
 public interface AIClient {
 
-    default CompletableFuture<ManipulationOutput> manipulate(String userMessage, Path path, String content) {
-        return manipulate(Optional.empty(), userMessage, path, content);
+    // Workspace update (manipulate)
+    default CompletableFuture<ManipulationOutput> workspaceUpdate(String userMessage, Path path, String content) {
+        return workspaceUpdate(Optional.empty(), userMessage, Map.of(), path, content);
     }
 
-    public CompletableFuture<ManipulationOutput> manipulate(Optional<String> systemMessage, String userMessage,
-            Path path, String content);
-
-    default CompletableFuture<GenerationOutput> generate(String userMessage, Path path, String content) {
-        return generate(Optional.empty(), userMessage, path, content);
+    default CompletableFuture<ManipulationOutput> workspaceUpdate(Optional<String> systemMessage, String userMessage,
+            Path path, String content) {
+        return workspaceUpdate(systemMessage, userMessage, Map.of(), Map.of(path, content));
     }
 
-    public CompletableFuture<GenerationOutput> generate(Optional<String> systemMessage, String userMessage,
-            Path path, String content);
-
-    default CompletableFuture<InterpretationOutput> interpret(String userMessage, Path path, String content) {
-        return interpret(Optional.empty(), userMessage, path, content);
+    default CompletableFuture<ManipulationOutput> workspaceUpdate(String userMessage, Map<Path, String> pathAndContent) {
+        return workspaceUpdate(Optional.empty(), userMessage, Map.of(), pathAndContent);
     }
 
-    public CompletableFuture<InterpretationOutput> interpret(Optional<String> systemMessage, String userMessage,
-            Path path, String content);
+    default CompletableFuture<ManipulationOutput> workspaceUpdate(Optional<String> systemMessage, String userMessage,
+            Map<Path, String> pathAndContent) {
+        return workspaceUpdate(systemMessage, userMessage, Map.of(), pathAndContent);
+    }
 
+    default CompletableFuture<ManipulationOutput> workspaceUpdate(String userMessage, Map<String, String> variables, Path path,
+            String content) {
+        return workspaceUpdate(Optional.empty(), userMessage, variables, path, content);
+    }
+
+    default CompletableFuture<ManipulationOutput> workspaceUpdate(Optional<String> systemMessage, String userMessage,
+            Map<String, String> variables,
+            Path path, String content) {
+        return workspaceUpdate(systemMessage, userMessage, variables, Map.of(path, content));
+    }
+
+    default CompletableFuture<ManipulationOutput> workspaceUpdate(String userMessage, Map<String, String> variables,
+            Map<Path, String> pathAndContent) {
+        return workspaceUpdate(Optional.empty(), userMessage, variables, pathAndContent);
+    }
+
+    public CompletableFuture<ManipulationOutput> workspaceUpdate(Optional<String> systemMessage, String userMessage,
+            Map<String, String> variables, Map<Path, String> pathAndContent);
+
+    // Workspace create (generate)
+    default CompletableFuture<GenerationOutput> workspaceCreate(String userMessage, Path path, String content) {
+        return workspaceCreate(Optional.empty(), userMessage, Map.of(), path, content);
+    }
+
+    default CompletableFuture<GenerationOutput> workspaceCreate(Optional<String> systemMessage, String userMessage,
+            Path path, String content) {
+        return workspaceCreate(systemMessage, userMessage, Map.of(), Map.of(path, content));
+    }
+
+    default CompletableFuture<GenerationOutput> workspaceCreate(String userMessage, Map<Path, String> pathAndContent) {
+        return workspaceCreate(Optional.empty(), userMessage, Map.of(), pathAndContent);
+    }
+
+    default CompletableFuture<GenerationOutput> workspaceCreate(Optional<String> systemMessage, String userMessage,
+            Map<Path, String> pathAndContent) {
+        return workspaceCreate(systemMessage, userMessage, Map.of(), pathAndContent);
+    }
+
+    default CompletableFuture<GenerationOutput> workspaceCreate(String userMessage, Map<String, String> variables, Path path,
+            String content) {
+        return workspaceCreate(Optional.empty(), userMessage, variables, path, content);
+    }
+
+    default CompletableFuture<GenerationOutput> workspaceCreate(Optional<String> systemMessage, String userMessage,
+            Map<String, String> variables,
+            Path path, String content) {
+        return workspaceCreate(systemMessage, userMessage, variables, Map.of(path, content));
+    }
+
+    default CompletableFuture<GenerationOutput> workspaceCreate(String userMessage, Map<String, String> variables,
+            Map<Path, String> pathAndContent) {
+        return workspaceCreate(Optional.empty(), userMessage, variables, pathAndContent);
+    }
+
+    public CompletableFuture<GenerationOutput> workspaceCreate(Optional<String> systemMessage, String userMessage,
+            Map<String, String> variables,
+            Map<Path, String> pathAndContent);
+
+    // Workspace Read (interpret)
+    default CompletableFuture<InterpretationOutput> workspaceRead(String userMessage, Path path, String content) {
+        return workspaceRead(Optional.empty(), userMessage, Map.of(), path, content);
+    }
+
+    default CompletableFuture<InterpretationOutput> workspaceRead(Optional<String> systemMessage, String userMessage,
+            Path path, String content) {
+        return workspaceRead(systemMessage, userMessage, Map.of(), Map.of(path, content));
+    }
+
+    default CompletableFuture<InterpretationOutput> workspaceRead(String userMessage, Map<Path, String> pathAndContent) {
+        return workspaceRead(Optional.empty(), userMessage, Map.of(), pathAndContent);
+    }
+
+    default CompletableFuture<InterpretationOutput> workspaceRead(Optional<String> systemMessage, String userMessage,
+            Map<Path, String> pathAndContent) {
+        return workspaceRead(systemMessage, userMessage, Map.of(), pathAndContent);
+    }
+
+    default CompletableFuture<InterpretationOutput> workspaceRead(String userMessage, Map<String, String> variables, Path path,
+            String content) {
+        return workspaceRead(Optional.empty(), userMessage, variables, path, content);
+    }
+
+    default CompletableFuture<InterpretationOutput> workspaceRead(Optional<String> systemMessage, String userMessage,
+            Map<String, String> variables,
+            Path path, String content) {
+        return workspaceRead(systemMessage, userMessage, variables, Map.of(path, content));
+    }
+
+    default CompletableFuture<InterpretationOutput> workspaceRead(String userMessage, Map<String, String> variables,
+            Map<Path, String> pathAndContent) {
+        return workspaceRead(Optional.empty(), userMessage, variables, pathAndContent);
+    }
+
+    public CompletableFuture<InterpretationOutput> workspaceRead(Optional<String> systemMessage, String userMessage,
+            Map<String, String> variables, Map<Path, String> pathAndContent);
+
+    // Workspace Dynamic (interpret)
+    default CompletableFuture<DynamicOutput> workspaceDynamic(String userMessage, Map<String, String> variables, Path path,
+            String content) {
+        return workspaceDynamic(Optional.empty(), userMessage, variables, path, content);
+    }
+
+    default CompletableFuture<DynamicOutput> workspaceDynamic(Optional<String> systemMessage, String userMessage,
+            Map<String, String> variables,
+            Path path, String content) {
+        return workspaceDynamic(systemMessage, userMessage, variables, Map.of(path, content));
+    }
+
+    default CompletableFuture<DynamicOutput> workspaceDynamic(String userMessage, Map<String, String> variables,
+            Map<Path, String> pathAndContent) {
+        return workspaceDynamic(Optional.empty(), userMessage, variables, pathAndContent);
+    }
+
+    public CompletableFuture<DynamicOutput> workspaceDynamic(Optional<String> systemMessage, String userMessage,
+            Map<String, String> variables,
+            Map<Path, String> pathAndContent);
+
+    // Exception
     default CompletableFuture<ExceptionOutput> exception(String userMessage, String stacktrace, Path path, String content) {
         return exception(Optional.empty(), userMessage, stacktrace, path, content);
     }
 
     public CompletableFuture<ExceptionOutput> exception(Optional<String> systemMessage, String userMessage, String stacktrace,
             Path path, String content);
+
+    // Dynamic (generic)
+    default CompletableFuture<DynamicOutput> dynamic(String userMessage) {
+        return dynamic(Optional.empty(), userMessage, Map.of());
+    }
+
+    default CompletableFuture<DynamicOutput> dynamic(Optional<String> systemMessage, String userMessage) {
+        return dynamic(systemMessage, userMessage, Map.of());
+    }
 
     default CompletableFuture<DynamicOutput> dynamic(String userMessage, Map<String, String> variables) {
         return dynamic(Optional.empty(), userMessage, variables);
--- core/deployment/src/main/java/io/quarkus/deployment/dev/ai/DynamicOutput.java
+++ core/deployment/src/main/java/io/quarkus/deployment/dev/ai/DynamicOutput.java
@@ -1,9 +1,19 @@
 package io.quarkus.deployment.dev.ai;
 
-public record DynamicOutput(String jsonResponse) {
+public record DynamicOutput<T>(T response) {
 
     @Override
     public String toString() {
-        return jsonResponse;
+        if (response == null) {
+            return null;
+        }
+        // For primitives, their wrappers, and String, return the simple string.
+        if (response instanceof String ||
+                response instanceof Number ||
+                response instanceof Boolean ||
+                response instanceof Character) {
+            return String.valueOf(response);
+        }
+        return response.toString(); // We don't have a Object->Json lib available here. Maybe move to vertx-http ?
     }
 }
